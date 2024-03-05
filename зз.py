c27c6fda2bf0d4f824d77be9e68c015b37623bd0
Nazik228pro <157514439+Nazik228pro@users.noreply.github.com>
2024-03-05 15:50:13 +0000

    HM 7

--- зз.py
+++ зз.py
@@ -1,20 +1,20 @@
-# Enter a word to check
-word = input("Enter word: ")
- 
-# Convert the word to lowercase to simplify verification
+# Input word for checking
+word = input("Enter a word: ")
+
+# Convert the word to lowercase for simplification of the check
 word = word.lower()
- 
+
 # Variable to store the result of the check
 is_palindrome = True
- 
-# Loop to check palindrome
+
+# Loop for palindrome check
 for i in range(len(word) // 2):
     if word[i] != word[len(word) - i - 1]:
         is_palindrome = False
         break
- 
+
 # Output the result
 if is_palindrome:
-    print("Word", word, "is a palindrome.")
+    print("The word", word, "is a palindrome.")
 else:
-    print("Word", word, "is not a palindrome.")
+    print("The word", word, "is not a palindrome.")
