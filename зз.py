6a30e7e6be3029603e80c16a6f7b439d3e9c59c9
Nazik228pro <157514439+Nazik228pro@users.noreply.github.com>
2024-03-05 15:56:52 +0000

    kj

--- зз.py
+++ зз.py
@@ -1,20 +1,20 @@
 # Input word for checking
-word = input("Enter a word: ")
+word = input("pupok: ")
 
 # Convert the word to lowercase for simplification of the check
 word = word.lower()
 
 # Variable to store the result of the check
-is_palindrome = True
+is_pupok = True
 
-# Loop for palindrome check
+# Loop for pupok check
 for i in range(len(word) // 2):
     if word[i] != word[len(word) - i - 1]:
-        is_palindrome = False
+        is_pupok = False
         break
 
 # Output the result
-if is_palindrome:
-    print("The word", word, "is a palindrome.")
+if is_pupok:
+    print("The word", word, "is a pupok.")
 else:
-    print("The word", word, "is not a palindrome.")
+    print("The word", word, "is not a pupok.")
